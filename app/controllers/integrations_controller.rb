2ae3469eca1546a8135ad56e983b4d0525f7ec05
Blake Strickland <Stricklandb02@gmail.com>
2016-02-29 09:58:08 -0500

    Better display

--- app/controllers/integrations_controller.rb
+++ app/controllers/integrations_controller.rb
@@ -2,27 +2,22 @@ class IntegrationsController < ApplicationController
   def show
     i = Integration.new(abv: params[:abv], artist: params[:artist])
     albums = []
-
+    url = []
     i.albums.each do |x|
       x[1]['items'].each do |i|
         albums << i['name']
+        url << i['external_urls']['spotify']
       end
+      @url = url
     end
-    url = []
-    i.albums.each do |x|
-      x[1]['items'].each do |x|
-        url << x['external_urls']['spotify']
-      end
-    @url = url
-  end
-
-
     @albums = albums.uniq
     beer_info = []
-
+    beer_description = []
     i.brew_api['data'].each do |x|
       beer_info << "#{x["name"]} @ #{x["abv"]} abv."
+      beer_description << "#{x["description"]}"
       @beer_info = beer_info.uniq
+      @beer_description = beer_description.uniq
     end
 
     @a = @albums
@@ -34,7 +29,10 @@ class IntegrationsController < ApplicationController
     @b = @beer_info.sample
 
     number = rand(0..@length)
-    @url = "#{@a[number]}; \n #{@u[number]}"
+    @album = "#{@a[number]}"
+    @url = "#{@u[number]}"
+    @description = "#{@beer_description[number]}"
+
   end
 
   def index
--- app/models/integration.rb
+++ app/models/integration.rb
@@ -1,18 +1,9 @@
 class Integration
   attr_reader :brew_api, :name, :albums
   def initialize(abv: nil,artist: nil)
-    # @song_api = HTTParty.get("https://api.spotify.com/v1/search?q=%22#{artist}%22&type=artist")
-
     @brew_api = HTTParty.get("http://api.brewerydb.com/v2/beers/?abv=#{abv}&key=#{ENV['BEER_KEY']}")
 
     @albums = HTTParty.get("https://api.spotify.com/v1/search?q=%22#{artist}%22&type=album")
-
-    # @beers = HTTParty.get("")
-
   end
 
-  def album_results
-    @genre = @song_api['artists']['items'].first['genres']
-    # @albums = HTTParty.get("https://api.spotify.com/v1/search?q=#{@name}&%22&type=artist")
-  end
 end
