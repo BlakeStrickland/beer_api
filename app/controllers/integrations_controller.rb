6c6d2ecf986a2ec3e1866ed4b8e4536ca21ad9d4
Blake Strickland <Stricklandb02@gmail.com>
2016-02-29 00:23:38 -0500

    Integration views

--- app/controllers/integrations_controller.rb
+++ app/controllers/integrations_controller.rb
@@ -2,11 +2,21 @@ class IntegrationsController < ApplicationController
   def show
     i = Integration.new(abv: params[:abv], artist: params[:artist])
     albums = []
+
     i.albums.each do |x|
       x[1]['items'].each do |i|
         albums << i['name']
       end
     end
+    url = []
+    i.albums.each do |x|
+      x[1]['items'].each do |x|
+        url << x['external_urls']['spotify']
+      end
+    @url = url
+  end
+
+
     @albums = albums.uniq
     beer_info = []
 
@@ -15,12 +25,16 @@ class IntegrationsController < ApplicationController
       @beer_info = beer_info.uniq
     end
 
-    a = @albums.sample
-    b = @beer_info.sample
+    @a = @albums
+    @u = @url
+    a_and_u = {}
+    @album_with_url = @a
+    @length = @a.count.to_i
 
-    s = "#{a} with a(n) #{b}"
+    @b = @beer_info.sample
 
-    render json: s
+    number = rand(0..@length)
+    @url = "#{@a[number]}; \n #{@u[number]}"
   end
 
   def index
